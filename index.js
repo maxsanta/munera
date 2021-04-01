e26c23ec1fe574ee0a464cbc5bb717c636b5d046
Maximiliano Santander <42254048+maxsanta@users.noreply.github.com>
2021-03-31 22:39:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//get the form by its id
+const form = document.getElementById("contact-form"); 
+
+//1.
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //2.
+  let mail = new FormData(form);
+
+  //3.
+  sendMail(mail);
+})
